57fc3f4117ba00ae765adef2ed9a35f4c84faf9d
Juliano Ramos <jpachecoramos@gmail.com>
2022-01-10 14:09:07 -0300

    Operacao de Instanciacao da Base de Dados

--- src/main/java/br/com/fasttrack/course/resources/UserResource.java
+++ src/main/java/br/com/fasttrack/course/resources/UserResource.java
@@ -1,7 +1,5 @@
 package br.com.fasttrack.course.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
